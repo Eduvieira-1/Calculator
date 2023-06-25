21f954677d64e02551eaad39350a86c3fc4ef524
Eduvieira-1 <edu.vieira1@icloud.com>
2023-06-25 17:56:07 -0300

    Projeto finalizado

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator.vue'
+
+export default {
+  components: {Calculator}
+}
+</script>
+
+<style>
+
+body{
+  margin: 0
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  background: linear-gradient(to right, #000000, #434343);
+}
+</style>
